bb4b54d3cda3fe2a82db1c211d4decdf127df1f3
ishahtech <salawudeenaishatolabisi@gmail.com>
2024-03-07 13:39:36 +0100

    fix:add the feedback favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
-// import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Header from "./Component/Layout/Header";
+import Home from "./components/Pages/Home";
+import Review from "./components/Pages/Review";
+import About from "./components/Pages/About";
 import data from "./data/reviewData";
 import ReviewList from "./Component/ReviewList";
 import ReviewStats from "./Component/ReviewStats";
@@ -18,13 +21,16 @@ function App() {
     
   return (
     <>
-    {/* <Router>
-      <Routes> 
-        <Route> </Route>
-      </Routes>
-       </Router> */}
-
       <Header text="Review Application" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/review" element={<Review />} />
+        </Routes>
+      </Router>
+
+
       <div className="container">
         <ReviewStats reviews={review} />
         <ReviewList reviews={review} deleteReview={deleteReview} />
